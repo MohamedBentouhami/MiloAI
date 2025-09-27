48b20abd1d80463b0d93af7c5aa3fd58f88bbf63
Mohamed <mohamedbentouhami512@gmail.com>
2025-09-27 21:24:51 +0200

    manage OpenAI API key

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import type { Request, Response } from "express";
+
 const app = express();
 const port = process.env.PORT || 3000;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello world')
+    res.send('Hello world');
 });
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-
 });
 
