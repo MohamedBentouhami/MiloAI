c58643d8258bd84816847f26255979553379f609
Mohamed <mohamedbentouhami512@gmail.com>
2025-10-05 20:46:06 +0200

    Add chat post route

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -10,7 +10,6 @@ function App() {
       .then(data => setMessage(data.message))
   }, []);
   return (<div className="p-4">
-
     <p className="font-bold text-3xl">{message}</p>
     <Button>Click me</Button>
   </div>)
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,17 +1,44 @@
 import express from "express";
 import type { Request, Response } from "express";
+import OpenAI from "openai";
+
+
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const client = new OpenAI({
+    apiKey: OPENAI_API_KEY
+})
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello world');
 });
 
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({message: 'Hello world'});
+    res.json({ message: 'Hello world' });
 });
+
+const conversations = new Map<string, string>();
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+    const { prompt, conversationId } = req.body;
+
+    const response = await client.responses.create({
+        model: 'gpt-4o-mini',
+        input: prompt,
+        temperature: 0.2,
+        max_output_tokens: 100,
+        previous_response_id: conversations.get(conversationId)
+    })
+
+    conversations.set(conversationId, response.id)
+
+    res.json({ message: response.output_text });
+
+})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
