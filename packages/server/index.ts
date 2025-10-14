20270ad4d540cee9902b587dd8684dcf06c8e0c6
Mohamed <mohamedbentouhami512@gmail.com>
2025-10-14 19:34:16 +0200

    add chat controller

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,7 +1,5 @@
 import express from "express";
-import type { Request, Response } from "express";
-import z from "zod";
-import { chatService } from "./services/chat.service";
+import { chatController } from "./controllers/chat.controller";
 
 
 
@@ -10,31 +8,8 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 
+app.post('/api/chat', chatController.sendMessage);
 
-const chatSchema = z.object({
-    prompt: z.string()
-        .trim()
-        .min(1, "Prompt is required")
-        .max(1000, "Prompt is too long (max 1000 characters)"),
-    conversationId: z.uuid()
-})
-
-app.post('/api/chat', async (req: Request, res: Response) => {
-    const { prompt, conversationId } = req.body;
-    const parseResult: any = chatSchema.safeParse(req.body);
-    if (!parseResult) {
-        res.status(400).json(parseResult.error.format());
-        return;
-    }
-
-    try {
-        const response = await chatService.sendMessage(prompt, conversationId);
-        res.json({ message: response.message });
-    } catch {
-        res.status(500).json({ error: 'Failed to generate a response' });
-    }
-
-})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
