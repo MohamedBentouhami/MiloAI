e0a748e8e871878133cf09c9f64c2f0bbd55216e
Mohamed <mohamedbentouhami512@gmail.com>
2025-10-15 08:21:09 +0200

    add ChatBox component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,11 @@
-import { useEffect, useState } from "react"
-import { Button } from "./components/ui/button";
+import ChatBox from "./components/ChatBox";
 
 function App() {
-
-  const [message, setMessage] = useState('')
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-  }, []);
-  return (<div className="p-4">
-    <p className="font-bold text-3xl">{message}</p>
-    <Button>Click me</Button>
-  </div>)
+  return (
+    <div className="p-4">
+      <ChatBox />
+    </div>
+  )
 
 }
 
---
+++ packages/client/src/components/ChatBox.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./ui/button"
+import { FaArrowUp } from "react-icons/fa"
+
+
+export default function ChatBox() {
+
+    return (<div className="flex flex-col items-end gap-2 border-2 p-4 rounded-3xl">
+        <textarea className="w-full border-0 focus:outline-0 resize-none" placeholder="Ask anything" maxLength={1000} />
+        <Button className="rounded-full w-9 h-9">
+            <FaArrowUp />
+        </Button>
+    </div>)
+}
