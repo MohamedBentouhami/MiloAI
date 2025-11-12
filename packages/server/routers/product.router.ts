23028665cf99c02570a3bd8e296db54355cb1111
Mohamed <mohamedbentouhami512@gmail.com>
2025-11-12 11:42:38 +0100

    add an endpoint to get reviews accoding a product

--- packages/server/routers/product.router.ts
+++ packages/server/routers/product.router.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { reviewController } from "../controllers/review.controller";
 
 const productRouter = Router();
 
-productRouter.get('/:id/reviews');
+productRouter.get('/:id/reviews', reviewController.getReviews);
 
 export default productRouter;
