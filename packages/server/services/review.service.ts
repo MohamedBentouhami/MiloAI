6524ecd08f2dfe4bc26fae437d0e48dbc6bbd8cc
Mohamed <mohamedbentouhami512@gmail.com>
2025-11-18 08:21:00 +0100

    extract prompt file

--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,6 +1,11 @@
 import { type Review } from "../generated/prisma";
 import { reviewRepository } from "../repositories/review.repository";
 import { llmClient } from "../llm/client";
+import {readFileSync} from "fs";
+import { join } from "path";
+
+const template = readFileSync(join(__dirname, "../prompts/test.txt"), "utf8");
+
 
 export const reviewService = {
     async getReviews(productId: number): Promise<Review[]> {
@@ -9,10 +14,7 @@ export const reviewService = {
     async summarizeReview(productId: number): Promise<string> {
         const reviews: Review[] = await reviewRepository.getReviews(productId);
         const joinedReviews = reviews.map(r => r.content).join('\n\n');
-        const prompt = `
-            Summarize the following customers reviews into a short paragraph highlighting kry themes, both positive and negative :
-            ${joinedReviews}
-            `;
+        const prompt = template.replace('{{reviews}}', joinedReviews);
 
         return llmClient.generateText({ model: "openai/gpt-oss-20b:free", prompt });
 
