297b75422f2774b41aed26a8b1a1f336e08dd318
Mohamed <mohamedbentouhami512@gmail.com>
2025-12-23 02:30:57 +0100

    implement tanstack for data fetching

--- packages/client/src/components/reviews/reviewList.tsx
+++ packages/client/src/components/reviews/reviewList.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
 import Skeleton from 'react-loading-skeleton'
+import { useQuery } from "@tanstack/react-query";
 
 type Props = {
     productId: number;
@@ -21,25 +22,15 @@ type GetReviewsResponse = {
 }
 
 export default function ReviewList({ productId }: Props) {
-    const [reviewData, setReviewData] = useState<GetReviewsResponse>();
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState('');
+    const { data: reviewData, error, isLoading } = useQuery<GetReviewsResponse>({
+        queryKey: ['reviews', productId],
+        queryFn: () => fetchReviews()
+    })
 
     const fetchReviews = async () => {
-        try {
-            setIsLoading(true);
-            const { data } = await axios.get<GetReviewsResponse>(`/api/products/${productId}/reviews`);
-            setReviewData(data);
-        } catch (error) {
-            console.error(error);
-            setError('Could not fetch the reviews. Try again!');
-        } finally {
-            setIsLoading(false);
-        }
+        const { data } = await axios.get<GetReviewsResponse>(`/api/products/${productId}/reviews`);
+        return data;
     }
-    useEffect(() => {
-        fetchReviews();
-    }, []);
 
     if (isLoading) {
         return (<div className="flex flex-col gap-5">
@@ -56,7 +47,7 @@ export default function ReviewList({ productId }: Props) {
         </div>)
     }
     if (error) {
-        return <p className="text-red-500">{error}</p>
+        return <p className="text-red-500">Could not fetch reviews. Try again! </p>
     }
 
     return (
